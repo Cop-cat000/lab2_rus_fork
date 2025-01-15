5a68002b8d02ca9f06f2ec479dc58770ce438a6f
Kraft143 <kraft143@bk.ru>
2025-01-16 02:16:56 +0600

    Уведомления отправлются в корректные топики. Перепроверяю условия с портацией и репликацией

--- patient/src/main/java/com/example/clinic/patient/controller/KafkaTopicPatientController.java
+++ patient/src/main/java/com/example/clinic/patient/controller/KafkaTopicPatientController.java
@@ -3,13 +3,9 @@ package com.example.clinic.patient.controller;
 import com.example.clinic.patient.service.KafkaTopicPatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
-@RestController
-@RequestMapping("/kafka") // Общий префикс для Kafka API
+@Component
 public class KafkaTopicPatientController implements CommandLineRunner {
 
     private final KafkaTopicPatientService kafkaTopicPatientService;
@@ -19,18 +15,6 @@ public class KafkaTopicPatientController implements CommandLineRunner {
         this.kafkaTopicPatientService = kafkaTopicPatientService;
     }
 
-    // Метод для создания топиков через REST API
-    @PostMapping("/create-topic")
-    public String createTopic(
-            @RequestParam String topicName,
-            @RequestParam int partitions,
-            @RequestParam short replicationFactor
-    ) {
-        kafkaTopicPatientService.createPatientTopic(topicName, partitions, replicationFactor);
-        return "Topic " + topicName + " created successfully";
-    }
-
-    // Метод для создания стандартных топиков при запуске приложения
     @Override
     public void run(String... args) throws Exception {
         kafkaTopicPatientService.createPatientTopic("patient-notifications", 3, (short) 3);
